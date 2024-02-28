602705818ea12556d15a59ba33778cb9d5591410
unknown <engr.jamatali@gmail.com>
2024-02-28 16:02:14 +0500

    Project Setup, cleaned, Tailwind comfigured and tested

---
+++ bg-change/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+   <div className='text-center p-10'>
+    <h1 className='text-blue-500'>Click Button to Change Background color</h1>
+
+   </div>
+
+   </>
+  )
+}
+
+export default App
