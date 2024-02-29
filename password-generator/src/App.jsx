8d432289ff69cd54155d574ad2df64bd59637686
unknown <engr.jamatali@gmail.com>
2024-02-29 14:39:07 +0500

    password Generator setup, tailwind and cleeanup

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <>
+   <h1 className='font-bold text-3xl p-4 text-center animate-bounce'>Password Generator</h1>
+   </>
+  )
+}
+
+export default App
