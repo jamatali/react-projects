3a76ff27bfd211739487b7c9c8932115fe53f323
unknown <engr.jamatali@gmail.com>
2024-05-03 16:54:13 +0500

    inputs added

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -21,7 +21,25 @@ const passwordGenerator = useCallback (() => {
    <h1 className='font-bold text-3xl p-4 text-center animate-bounce'>Password Generator</h1>
    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
-      <input type="text" value={password} className="outline-none w-full py-1 px-3" placeholder="password" readOnly/>
+      <input type="text" 
+      value={password} className="outline-none w-full py-1 px-3" 
+      placeholder="password" readOnly/>
+      <button className="Outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy</button>
+    </div>
+    <div className="flex text-sm gap-x-2">
+      <div className="flex items-center gap-x-1">
+        <input type="range"
+        min={8}
+        max={100} 
+        value={length}
+        className="cursor-pointer"
+        onChange={(e) => setLength(e.target.value)}/>
+        <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input type="checkbox" defaultChecked ={numberAllowed} onChange={(prev) => {setNumberAllowed(!prev)}}/>
+        <label>Numbers</label>
+      </div>
     </div>
    </div>
    </>
