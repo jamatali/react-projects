d3dd81f7547f643d06d1bae3b77d20bddd01c3c3
unknown <engr.jamatali@gmail.com>
2024-06-24 11:32:55 +0500

    handleChange

--- mr-16/src/App.jsx
+++ mr-16/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [num, setNum] = useState(0);
+
   const handleCount = () => {
-    setCount(count + 1);
+    setCount(count + num);
+  };
+  const handleChange = (e) => {
+    setNum(parseInt(e.target.value));
   };
   return (
     <>
       <h1>Counter App</h1>
       <button>Count: {count}</button>
+      <input type="number" value={num} onChange={handleChange} />
     </>
   );
 }
